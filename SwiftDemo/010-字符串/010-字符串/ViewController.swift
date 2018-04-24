d2b44219ff38ebb8492545e5c9095ac5340970ce
caihongguang <15052422@qq.com>
2018-04-24 17:00:01 +0800

    字符串子串

--- SwiftDemo/010-字符串/010-字符串/ViewController.swift
+++ SwiftDemo/010-字符串/010-字符串/ViewController.swift
@@ -4,7 +4,8 @@
 //
 //  Created by caihongguang on 2018/4/24.
 //  Copyright © 2018年 SYJ. All rights reserved.
-//
+//http://www.iqiyi.com/v_19rrb57voo.html#curid=643717700_529a0ab2d04b6d1de0baa44ab0ed11e6
+// 具体使用的时候用最新的 api . 字符串常用的一些 ，欢迎大家一起提供和提出问题
 
 import UIKit
 
@@ -13,9 +14,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        demo3()
+        demo4()
         
     }
+    //MARK: - 字符串的字串
+    func demo4() {
+        //建议： 一般使用 NSString 作为中转，因为Swift取子串的方法一直在优化
+        let str = "我们一起去飞"
+        //1. NSString
+        let ocStr = str as NSString
+        let s1 = ocStr.substring(with: NSMakeRange(2, 3))
+        print(s1)
+        
+        //2. String 的3.0方法
+//        let r = 0..<5
+        //str.substring(with:xxx)
+        //startIndex position == 0
+        print(str.startIndex)
+        //endIndex postion == str.length
+        print(str.endIndex)
+        
+        //4.0废弃
+        str.substring(from: "我们".endIndex)
+        
+        
+
+        
+    }
+    
     
     //MARK: - 格式化
     func demo3() -> (){
