7d4f6b0cd70344d91296dfb2429b694bfc92977e
JayShang <yijieshang@gmail.com>
2018-04-27 15:38:24 +0800

    通过闭包回调传递参数

--- 002-Swift面向对象Demo/004-GCD/004-GCD/ViewController.swift
+++ 002-Swift面向对象Demo/004-GCD/004-GCD/ViewController.swift
@@ -13,18 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        loadData{ (result) in
+            print("获取新闻数据 \(result)")
+        }
         
     }
     
+    //闭包的应用场景和OC的block完全一致
     //看官方文档和最新的 WWDC 然后搜关键字，有文档幻灯片 视频
-    func loadData() -> () {
+    func loadData(completion : (_ result: [String]) -> () ) -> () {
+        
+       
         
         //将任务添加到队列，指定执行任务的函数
         //翻译 '队列' 调度任务(block/闭包) 以 同步/异步 的方式执行
         DispatchQueue.global().async {
             print("耗时操作 \(Thread.current)")
        
+            
+            
+            //休眠 睡一秒
+            Thread.sleep(forTimeInterval: 1.0)
+            //获得结果
+            let json = ["头","ba","网络数据"]
+            
+            
+            
                 //主队列回调
                 DispatchQueue.main.async(execute:{
                     print("主线程更新 UI")
