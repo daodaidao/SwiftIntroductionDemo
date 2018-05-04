49995a72a725a42e46fd4d402da29bc4caa74dfe
JayShang <yijieshang@gmail.com>
2018-05-04 11:32:47 +0800

    加载百度数据

--- 002-Swift面向对象Demo/010-运行时加载属性列表（有问题）/010-运行时加载属性列表/ViewController.swift
+++ 002-Swift面向对象Demo/010-运行时加载属性列表（有问题）/010-运行时加载属性列表/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  010-运行时加载属性列表
-//
+//http://www.iqiyi.com/v_19rrb41whc.html#curid=643841500_dd1151b53352e462d27d5300169b6d9f
 //  Created by caihongguang on 2018/5/4.
 //  Copyright © 2018年 SYJ. All rights reserved.
 //
---
+++ 002-Swift面向对象Demo/加载百度/加载百度/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  加载百度
+//https://blog.csdn.net/wangjunling888/article/details/50980964
+//设置支持http ats
+//  Created by caihongguang on 2018/5/4.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let url = URL(string: "http://www.baidu.com")
+        
+        //发起网络请求
+        // - 和 OC的区别，闭包的所有参数，需要自己写， OC直接带入
+        //- 如果不关心的值，可以直接 '_' 忽略
+        URLSession.shared.dataTask(with: url!){ (data,_,_) in
+            
+//            if (error != nil)
+//            {
+//                print("网络请求失败")
+//                return
+//            }
+            guard let data = data else {
+                print("网络请求失败")
+                return
+            }
+            
+            //将data 转换为String
+            let html = String(data: data, encoding: .utf8)
+      
+            print(html)
+        }.resume()
+        
+        
+        
+    }
+
+ 
+
+}
+
